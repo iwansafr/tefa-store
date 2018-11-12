6df79307394edcd61b2543f4bd7f827c8ce80f57
iwansafr <iwansafr@gmail.com>
2018-11-12 15:11:40 +0700

    manual add menu for menu bottom

--- application/modules/admin/views/manual/add_menu.php
+++ application/modules/admin/views/manual/add_menu.php
@@ -22,4 +22,31 @@
 			<img src="<?php echo image_module('manual','add_menu/8.png') ?>">
 		</p>
 	</ol>
+	<li>Menu Bottom</li>
+	<img src="<?php echo image_module('manual','add_menu/9.png') ?>">
+	<ol>
+		<li>pilih menu menu > menu position</li>
+		<img src="<?php echo image_module('manual','add_menu/10.png') ?>">
+		<li>tambah menu position sesuai judul yang diinginkan</li>
+		<img src="<?php echo image_module('manual','add_menu/11.png') ?>">
+		<img src="<?php echo image_module('manual','add_menu/12.png') ?>">
+		<li>kemudian buka menu content > content list</li>
+		<p>karena kita akan menambahkan content atau artikel ke dalam menu</p>
+		<img src="<?php echo image_module('manual','add_menu/13.png') ?>">
+		<p>dari gambar di atas kita tahu bahwa content dg judul contact us akan kita jadikan menu dengan cara klik judul content</p>
+		<li>klik tombol add to menu</li>
+		<img src="<?php echo image_module('manual','add_menu/14.png') ?>">
+		<li>isi kolom pada menu sesuai gambar di bawah ini</li>
+		<img src="<?php echo image_module('manual','add_menu/15.png') ?>">
+		<p>selanjutnya kita harus memilih menu position yang akan di tampilkan</p>
+		<li>menuju menu configuration > templates > manage widget</li>
+		<img src="<?php echo image_module('manual','header/2.png') ?>">
+		<li>kemudian cari judul widget di paling bawah yang berjudul menu bottom</li>
+		<img src="<?php echo image_module('manual','add_menu/16.png') ?>">
+		<p>isi content dengan memilih menu position yang sudah kita buat tadi dan sudah kita tambahkan menu di dalamnya</p>
+		<p>kemudian klik tombol save paling bawah</p>
+		<li>hasil dari penambahan menu</li>
+		<img src="<?php echo image_module('manual','add_menu/17.png') ?>">
+		<p>note : untuk menambahkan menu lagi di tempat yang sama, tidak perlu menambahkan menu position, tapi langsung saja menambah menu lewat content list</p>
+	</ol>
 </ol>
